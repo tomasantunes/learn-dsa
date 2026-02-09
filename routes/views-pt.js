d039aa4e2016035019b79192bae8006295a980f4
Tomás Antunes <tomasantunes@gmail.com>
2026-02-09 08:32:09 +0000

    Fix errors.

--- routes/views-pt.js
+++ routes/views-pt.js
@@ -9,4 +9,20 @@ router.get('/pt/computational-complexity', function(req, res, next) {
   res.render('content/pt/computational-complexity');
 });
 
+router.get('/pt/linked-lists-intro', function(req, res, next) {
+  res.render('content/pt/linked-lists-intro');
+});
+
+router.get('/pt/singly-linked-lists', function(req, res, next) {
+  res.render('content/pt/singly-linked-lists');
+});
+
+router.get('/pt/doubly-linked-lists', function(req, res, next) {
+  res.render('content/pt/doubly-linked-lists');
+});
+
+router.get('/pt/circular-linked-lists', function(req, res, next) {
+  res.render('content/pt/circular-linked-lists');
+});
+
 module.exports = router;
